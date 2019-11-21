1f7c3fffac928b9d2be0be42c28517c1417f2215
Lewis Nozaki <lhnozaki@gmail.com>
2019-11-20 21:34:47 -1000

    setting up backend

---
+++ server/knexfile.js
@@ -0,0 +1,24 @@
+const path = require("path");
+require("dotenv").config({ path: "../.env" });
+
+module.exports = {
+  client: "pg",
+  connection: {
+    host: process.env.POSTGRES_HOSTNAME,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_CONTAINER_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: path.join(__dirname, "database", "migrations")
+  },
+  seeds: {
+    directory: path.join(__dirname, "database", "seeds")
+  }
+};
