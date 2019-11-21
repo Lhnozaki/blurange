ec98d23f92ba0d1474aac25d7750034d4d7bc0e0
frzrbox <david@freezerbox.dev>
2019-11-20 21:54:32 -1000

    updates styles

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import './App.scss';
 import SplashPage from '../Components/SplashPage';
 import Header from '../Components/Header';
+import './App.scss';
 
 function App() {
   const [isAuth, setAuth] = useState(false);
 
   return (
     <div className="App">
-      <Header />
+      <Header setAuth={setAuth} isAuth={isAuth} />
       {isAuth ? <h1>Authorized</h1> : <SplashPage />}
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './Header.module.scss'
-const Header = () => {
+const Header = ({ isAuth, setAuth }) => {
     return (
         <header className={styles.header}>
             <div className="sitebranding">blurange</div>
             <div className="navigation">
-                <button>login</button>
+                <button onClick={() => setAuth(!isAuth)}>{isAuth ? 'logout' : 'login'}</button>
                 <button>sign up</button>
             </div>
         </header>
