981925093d522cd28d37dd494f27b59d5139b85a
frzrbox <david@freezerbox.dev>
2019-11-23 10:33:44 -1000

    creates editor and timeline components

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Timeline from '../Timeline';
+import styles from './Editor.module.scss';
 
 const Editor = () => {
     return (
-        <div>Editor</div>
+        <div className={styles.editor}>
+            <Timeline />
+            this is the editor
+        </div>
     )
 }
 
---
+++ src/Components/Timeline/Timeline.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Timeline.module.scss'
+
+const Timeline = () => {
+    return (
+        <div className={styles.progressBar}></div>
+    )
+}
+
+export default Timeline;
