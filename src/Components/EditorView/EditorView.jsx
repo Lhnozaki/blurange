80da003d8b3977f36b7c1a15ae9f9d9e2089302e
frzrbox <david@freezerbox.dev>
2019-11-23 17:24:52 -1000

    adds dropshadow to the custom scrollbar

--- src/Components/EditorView/EditorView.jsx
+++ src/Components/EditorView/EditorView.jsx
@@ -5,6 +5,10 @@ const EditorView = () => {
     return (
         <div className={styles.editorView}>
             <section><h1>;asldkjfa;lsdfkj</h1></section>
+            <section><h1>;asldkjfa;lsdfkj</h1></section>
+            <section><h1>;asldkjfa;lsdfkj</h1></section>
+            <section><h1>;asldkjfa;lsdfkj</h1></section>
+
         </div>
     )
 }
