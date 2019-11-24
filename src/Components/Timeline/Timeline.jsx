76d15fb77538cd0d09258c9c242663bc263197ce
frzrbox <david@freezerbox.dev>
2019-11-23 16:23:55 -1000

    restyles the timeline

--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -3,7 +3,7 @@ import styles from './Timeline.module.scss'
 
 const Timeline = () => {
     return (
-        <div>
+        <div className={styles.timeline}>
             <p>You're 50% finished</p>
             <div className={styles.progressBar}></div>
         </div>
