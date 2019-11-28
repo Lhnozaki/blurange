0b709c1e150f74a4dad9eded6a37e4f255302dee
frzrbox <david@freezerbox.dev>
2019-11-27 20:26:53 -1000

    fixes imports

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,34 +2,34 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import SplashPage from '../Components/SplashPage';
 import DesignSystem from '../Components/DesignSystem';
-import Editor from '../Components/Editor';
-import Dashboard from "../Components/Dashboard";
+import Editor from '../Components/EditorComponents/Editor';
+import Dashboard from "../Components/DashboardComponents/Dashboard";
 import TemplatesList from '../Components/TemplatesList';
 import JTemplate1 from '../Components/Templates/JTemplate1';
 
 
 export const routes = [
-    {
-        path: '/',
-        exact: true,
-        component: SplashPage
-    },
-    {
-        path: '/design-system',
-        exact: true,
-        component: DesignSystem
-    },
-    {
-        path: '/editor',
-        exact: true,
-        component: Editor
-    },
-    {
-        path: '/dashboard',
-        exact: true,
-        component: Dashboard
-    },
-    {
+  {
+    path: '/',
+    exact: true,
+    component: SplashPage
+  },
+  {
+    path: '/design-system',
+    exact: true,
+    component: DesignSystem
+  },
+  {
+    path: '/editor',
+    exact: true,
+    component: Editor
+  },
+  {
+    path: '/dashboard',
+    exact: true,
+    component: Dashboard
+  },
+  {
     path: '/jtemplate1',
     exact: true,
     component: JTemplate1
@@ -43,20 +43,20 @@ export const routes = [
 
 // Create auth routes and render if authorized in the home page
 export const authRoutes = [
-    {
-        path: '/',
-        exact: true,
-    },
-    {
-        path: '/design-system',
-        exact: true,
-        component: DesignSystem
-    },
-    {
-        path: '/editor',
-        exact: true,
-        component: Editor
-    }
+  {
+    path: '/',
+    exact: true,
+  },
+  {
+    path: '/design-system',
+    exact: true,
+    component: DesignSystem
+  },
+  {
+    path: '/editor',
+    exact: true,
+    component: Editor
+  }
 ]
 
 
