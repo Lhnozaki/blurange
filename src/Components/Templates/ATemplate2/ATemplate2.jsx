f98c252e9432fb5d2de6b2cd0534d59fb6dfa3ea
Andrew Obiano <andrew.obiano@gmail.com>
2019-12-02 15:23:05 -1000

    Added in sliding pages

--- src/Components/Templates/ATemplate2/ATemplate2.jsx
+++ src/Components/Templates/ATemplate2/ATemplate2.jsx
@@ -7,13 +7,21 @@ export default function ATemplate2() {
   return (
     <div id={styles.container} className="templateWrapper">
       <div className={styles.menu}>
-        <button className={styles.menuBtn}>Dropdown</button>
+        <button className={styles.menuBtn}>></button>
         <div className={styles.menuContent}>
           <Link to="/templates">Go back</Link>
           <Link to="/">Contact Me</Link>
           <Link to="/">GitHub</Link>
           <Link to="/">LinkedIn</Link>
         </div>
+      </div>
+
+      <div className={styles.slider}>
+        <div className={styles.slide}>Slide 1</div>
+        <div className={styles.slide}>Slide 2</div>
+        <div className={styles.slide}>Slide 3</div>
+        <div className={styles.slide}>Slide 4</div>
+        <div className={styles.slide}>Slide 5</div>
       </div>
     </div>
   );
