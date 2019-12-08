6997f433c866a7c95a80333c9b19496df4566eb9
Lewis Nozaki <lhnozaki@gmail.com>
2019-12-07 14:38:51 -1000

    injecting stripe with elements

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
-import styles from './Payment.module.scss';
+import React, { Component } from "react";
+import styles from "./Payment.module.scss";
+import { CardElement, injectStripe } from "react-stripe-elements";
 
 class Payment extends Component {
-    constructor() {
-        super();
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            validated: false
-        }
-
-        this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      validated: false,
+      Message: "",
+      success: false,
+      name: "",
+      email: "",
+      amount: 1000000
     };
 
-    validated() {
-        return (
-            <div>
-                <h2>payment successful!!!</h2>
-            </div>
-        )
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  validated() {
+    return (
+      <div>
+        <h2>payment successful!!!</h2>
+      </div>
+    );
+  }
+
+  renderPayments() {
+    if (this.state.validated) {
+      return this.validated();
+    } else {
+      return (
+        <div>
+          <h2>this is the payment modal</h2>
+        </div>
+      );
     }
+  }
 
-    renderPayments() {
-        if (this.state.validated) {
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ validated: !this.state.validated });
+  }
 
-            return this.validated()
-
-        } else {
-            return (
-                <div>
-                    <h2>this is the payment modal</h2>
-                </div>
-            )
-        }
-    }
-
-    handleClick(e) {
-        e.preventDefault();
-        this.setState({ validated: !this.state.validated });
-    }
-
-    render() {
-        return (
-            <div className={styles.paymentModal}>
-                {this.renderPayments()}
-
-                <button onClick={this.handleClick}>confirm price</button>
-            </div >
-        )
-    }
+  render() {
+    return (
+      <div className={styles.paymentModal}>
+        {this.renderPayments()}
+        <CardElement />
+        <button onClick={this.handleClick}>confirm price</button>
+      </div>
+    );
+  }
 }
 
-
-export default Payment;
+export default injectStripe(Payment);
