dd5ed77f89dce77512a644b98ac2f79eb74bbb5d
Kevin Guo <Kevin@Kevins-Air.home>
2019-12-20 15:18:31 -1000

    docker works now, we didnt need the route to the templates

--- server/server.js
+++ server/server.js
@@ -1,34 +1,33 @@
 ///// DOTENV  /////
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const decorator = require('./database/decorator');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const passport = require('passport');
-const methodOverride = require('method-override');
-const bcrypt = require('bcryptjs');
-const knex = require('./database/knex');
-const flash = require('connect-flash');
+const express = require("express");
+const cors = require("cors");
+const decorator = require("./database/decorator");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const methodOverride = require("method-override");
+const bcrypt = require("bcryptjs");
+const knex = require("./database/knex");
+const flash = require("connect-flash");
 
 ///// ROUTING /////
-const githubAuth = require('./routes/api/auth/github');
-const linkedinAuth = require('./routes/api/auth/linkedin');
-const paymentsRoute = require('./routes/api/payment');
-const atemplate1 = require('../templates/app');
+const githubAuth = require("./routes/api/auth/github");
+const linkedinAuth = require("./routes/api/auth/linkedin");
+const paymentsRoute = require("./routes/api/payment");
 
 ///// REDIS /////
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
+const RedisStore = require("connect-redis")(session);
+const redis = require("redis");
 // const client = redis.createClient({ url: process.env.REDIS_URL });
 
 ///// PORT /////
 const PORT = process.env.EXPRESS_HOST_PORT;
 
 //Config passport
-const passportConfig = require('./config/passport');
+const passportConfig = require("./config/passport");
 
 ///// APP /////
 const app = express();
@@ -39,7 +38,7 @@ app.use(express.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(decorator);
 app.use(passport.initialize());
 
@@ -51,20 +50,18 @@ app.use(
   })
 );
 
-app.use('/api/auth/github', githubAuth);
-app.use('/api/auth/linkedin', linkedinAuth);
-app.use('/api/charge', paymentsRoute);
-app.use('/atemplate1', atemplate1);
-
+app.use("/api/auth/github", githubAuth);
+app.use("/api/auth/linkedin", linkedinAuth);
+app.use("/api/charge", paymentsRoute);
 
 ///// Smoke Test /////
 
-app.get('/', (req, res) => {
-  console.log('test61');
+app.get("/", (req, res) => {
+  console.log("test61");
   res.status(200).json({ success: true });
 });
-app.get('/smoke', (req, res) => {
-  console.log('test65');
+app.get("/smoke", (req, res) => {
+  console.log("test65");
 });
 
 ///// LISTEN /////
