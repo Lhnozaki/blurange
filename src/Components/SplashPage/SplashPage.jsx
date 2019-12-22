86561a5a20fe7a4eca23d67e232dd14c73a34485
frzrbox <david@freezerbox.dev>
2019-12-21 15:41:33 -1000

    finishes splash page

--- src/Components/SplashPage/SplashPage.jsx
+++ src/Components/SplashPage/SplashPage.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import styles from './SplashPage.module.scss';
 import SvgBanner from '../SvgBanner';
 import Gallery from '../Gallery';
+import feature1 from '../../assests/Splash-5.jpg';
+import feature2 from '../../assests/Splash-6.jpg';
+import feature3 from '../../assests/Splash-7.jpg';
 
 const SplashPage = () => {
   return (
@@ -40,29 +43,44 @@ const SplashPage = () => {
       </section>
       <section id={styles.aboutContent} className="scroll-element">
         <h3 className='title'>3 steps to go live</h3>
-        <div className={styles.cardContainer}>
-          <div >
-            <h3 className="color-blue">1. Pick a theme</h3>
-            <p>Take the stress of design off your mind. We have a multitude of different templates for you to choose from.</p>
+        <div className={styles.instructionContainer}>
+          <div className={styles.instruction}>
+            <div className={styles.illustration}>
+              <img src={feature1} alt="first feature"/>
+            </div>
+            <div className={styles.content}>
+              <h3 className="color-blue">1. Pick a theme</h3>
+              <p>Take the stress of design off your mind. We have a multitude of different templates for you to choose from.</p>
+            </div>
           </div>
-          <div >
-            <h3 className="color-blue">2. Keep it DRY</h3>
-            <p>Drop in a document, add it from <strong>Github</strong>, we'll fill out the template for you just change what you want.</p>
+          <div className={styles.instruction}>
+            <div className={styles.illustration}>
+              <img src={feature2} alt="second feature"/></div>
+            <div className={styles.content}>
+              <h3 className="color-blue">2. Keep it DRY</h3>
+              <p>Drop in a document, add it from <strong>Github</strong>, we'll fill out the template for you just change what you want.</p>
+            </div>
           </div>
-          <div >
-            <h3 className="color-blue">3. Make it live</h3>
-            <p>You can host it yourself or on our servers. We have custom domains too if you want them.</p>
+          <div className={styles.instruction}>
+            <div className={styles.illustration}>
+              <img src={feature3} alt="three feature"/></div>
+            <div className={styles.content}>
+              <h3 className="color-blue">3. Make it live</h3>
+              <p>You can host it yourself or on our servers. We have custom domains too if you want&nbsp;them.</p> 
+            </div>
           </div>
         </div>
       </section>
+      <section id={styles.bottomCta}>
+        <div className={styles.content}>
+          <h2 id={styles.ctaHeader} className="title">Test it out</h2>
+          <p>Try it out and deploy your site now. No credit card needed unless you want lol.</p>
+          <Link to="/editor/templates"><button className="cta">Get Started</button></Link>
+        </div>
+      </section>
+      <footer className={styles.footer}>&copy; 2020 blurange</footer>
     </article>
   )
 }
 
 export default SplashPage;
-
-
-// <section id={styles.gallery}> 
-// <Gallery/>
-// <h3>By <strong>Developers</strong> For <strong>Developers</strong></h3>
-// </section>
